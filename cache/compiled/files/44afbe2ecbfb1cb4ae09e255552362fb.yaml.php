76da1c3d26285635a64b4caf7c13da272992ab8e
adijayainc <trias.adijaya@gmail.com>
2023-10-01 10:53:19 +0700

    blogs site terbaru

---
+++ cache/compiled/files/44afbe2ecbfb1cb4ae09e255552362fb.yaml.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/blogs/system/blueprints/config/media.yaml',
+    'modified' => 1688067395,
+    'size' => 63,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.MEDIA',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => NULL
+        ]
+    ]
+];
